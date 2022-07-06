b4d90db8610cbc35776030e0a6fc5e200c18b17c
Александр Кобылянский <aleksandr.kobylyansky@murka.com>
2022-07-06 15:18:33 +0300

    - cleaning and creating subfolders - add altitude catching

--- JSON2CSV-Google_Location_History.py
+++ JSON2CSV-Google_Location_History.py
@@ -3,11 +3,15 @@ import re
 import json
 import csv
 import pathlib
+import subprocess
 
 JSON_data="Records.json"
 
+cmd_pre = subprocess.Popen("rm -rf csv/; rm -rf gpx/; mkdir csv gpx", stdout=subprocess.PIPE, shell=True)
+
 with open(JSON_data) as f:
     data = json.load(f)
+
 
 for x in range(len(data["locations"])):
     timestamp_pre=str(data['locations'][x]['timestamp'])
@@ -17,17 +21,22 @@ for x in range(len(data["locations"])):
     time=re.sub(r'Z', '', re.sub(r'\..*', '',timestamp[1]))
     lat=int(data['locations'][x]['latitudeE7'])/1e7
     lon=int(data['locations'][x]['longitudeE7'])/1e7
+    try:
+        alt=int(data['locations'][x]['altitude'])
+    except:
+        alt=1
     CSV_file="csv/Records_" + dateforfilename + ".csv"
     file = pathlib.Path(CSV_file)
     if file.exists ():
         csv_f=open(CSV_file, 'a')
-        csv_f.write(str(lat) + "," + str(lon) + "," + str(date) + "," + str(time) +'\n')
+        csv_f.write(str(lat) + "," + str(lon) + "," + str(alt) + "," + str(date) + "," + str(time) +'\n')
     else:
         csv_f=open(CSV_file, 'a')
-        csv_f.write('lat,lon,date,time\n')
-        csv_f.write(str(lat) + "," + str(lon) + "," + str(date) + "," + str(time) +'\n')
+        csv_f.write('lat,lon,alt,date,time\n')
+        csv_f.write(str(lat) + "," + str(lon) + "," + str(alt) + "," + str(date) + "," + str(time) +'\n')
     csv_f.close
-print('Done')    
 
-print('cd csv; for i in *.csv; do gpsbabel -i unicsv -t -f $i -o gpx -F ../gpx/$i.gpx; done; cd ..')
+
+cmd_babel = subprocess.Popen("cd csv; for i in *.csv; do gpsbabel -i unicsv -t -f $i -o gpx -F ../gpx/$i.gpx; done;", stdout=subprocess.PIPE, shell=True)
+print('Done')    
 
